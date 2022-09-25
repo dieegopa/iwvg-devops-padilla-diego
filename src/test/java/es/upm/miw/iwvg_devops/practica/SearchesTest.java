665f7cef0fdd6aa61c335d29d25ad1b1556bf781
Diego Padilla <dpadilla@neuromobilemarketing.com>
2022-09-25 21:11:28 +0200

    Added search 0 #11

--- src/test/java/es/upm/miw/iwvg_devops/practica/SearchesTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/practica/SearchesTest.java
@@ -2,6 +2,9 @@ package es.upm.miw.iwvg_devops.practica;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SearchesTest {
@@ -12,6 +15,12 @@ class SearchesTest {
         assertEquals(8, new Searches().findFractionDivisionByUserId("4").getDenominator());
         assertEquals(1, new Searches().findFractionDivisionByUserId("4").decimal());
         assertEquals(Fraction.class, new Searches().findFractionDivisionByUserId("4").getClass());
+    }
+
+    @Test
+    void testFindUserFamilyNameInitialBySomeProperFraction() {
+        assertEquals(List.of("F", "B", "L", "B"), new Searches().findUserFamilyNameInitialBySomeProperFraction()
+                .collect(Collectors.toList()));
     }
 
 }
